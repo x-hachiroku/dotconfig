9d06f8ee5bea70ae3da0ea6a9026408b9b216551
x-hachiroku <gh.x86@yukimura.moe>
2024-10-14 16:38:48 +0800

    キラキラ夢見よう
    夢を見ることに
    早すぎるも　遅すぎるもないから
    自分らしい夢を！

---
+++ zsh/systems/50-arch.zsh
@@ -0,0 +1,35 @@
+source env_parallel.zsh
+
+alias endsession='/usr/lib/qt6/bin/qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logout'
+alias reset-plasma='kquitapp6 plasmashell || killall plasmashell; kstart plasmashell >/dev/null'
+
+function open {
+    if [[ -e "$1" ]]; then
+        xdg-open "$1" >/dev/null 2>&1
+    else
+        return 233
+    fi
+}
+
+function trash {
+    kioclient move "$@" 'trash:/'
+}
+
+if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+    alias y='wl-copy --trim-newline'
+    alias p='wl-paste -t text --no-newline'
+else
+    alias y='xclip -selection c -r'
+    alias p='xclip -selection c -o'
+fi
+
+function termbl () {
+    sudo zsh -c "echo $(( $(cat /sys/class/backlight/*/max_brightness) * $1 / 100 )) > /sys/class/backlight/*/brightness"
+}
+
+
+if [[ $- =~ i ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]]; then
+    tmux attach-session -t ssh 2>/dev/null || tmux new-session -s ssh "archey4; zsh"
+fi
+
+alias startplasma-wayland='/usr/lib/plasma-dbus-run-session-if-needed /usr/bin/startplasma-wayland'
